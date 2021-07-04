5c035646902c027a1802ec7da7459c6d59e80e9a
Ziting Mao <32125728+MaoZain@users.noreply.github.com>
2021-07-04 15:45:09 -0500

    add cors

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ const port = process.env.PORT || config.port
 // error handler
 onerror(app)
 
+app.use(cors({
+  origin: '*', // 允许跨域的地址，我的理解类似白名单，*代表全部允许
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'], // 暴露header列表
+  maxAge: 5, // 每隔5秒发送预检请求，也就是发送两次请求
+  credentials: true, // 允许请求携带cookie
+  allowMethods: ['OPTIONS', 'GET', 'PUT', 'POST', 'DELETE'], // 请求方式
+  allowHeaders: ['Accept', 'Origin', 'Content-type', 'Authorization'],
+}))
+
 // middlewares
 app.use(bodyparser())
   .use(json())
@@ -41,10 +50,9 @@ app.use(bodyparser())
   }))
   .use(router.routes())
   .use(router.allowedMethods())
+
 // logger
 app.use(async (ctx, next) => {
-  ctx.set("Access-Control-Allow-Origin", "*");
-
   const start = new Date()
   await next()
   const ms = new Date() - start
@@ -52,12 +60,9 @@ app.use(async (ctx, next) => {
 })
 
 //cors
-app.use(cors())
 
-app.use(async (ctx, next) => {
-  ctx.set("Access-Control-Allow-Origin", "*")
-  await next()
-})
+
+
 
 
 router.post('/back', async (ctx, next) => {
